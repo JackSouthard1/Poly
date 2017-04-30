bf117c21021e091b2dbc1520a4da55c8af7a1040
Jack Southard <jacksouthard265@gmail.com>
2017-04-29 20:28:36 -0700

    Parts spawing and movement fix

---
+++ Assets/Scripts/PartSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+
+public class PartSpawner : NetworkBehaviour {
+
+	public GameObject partPrefab;
+//	private GameObject segments;
+
+	private float spawnTimer;
+	private float spawnInterval = 2f;
+	private int partCount = 0;
+	private int partCountMax = 5;
+	// Use this for initialization
+	void Start () {
+		spawnTimer = spawnInterval;
+	}
+	
+	void Update ()
+	{
+		if (spawnTimer <= 0f) {
+			spawnTimer = spawnInterval;
+			SpawnSegment ();
+		} else {
+			spawnTimer -= Time.deltaTime;
+		}
+	}
+	void SpawnSegment ()
+	{
+		if (partCount < partCountMax) {
+			var part = Instantiate (partPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+//				segment.transform.parent = this.gameObject.transform;
+			partCount++;
+
+			NetworkServer.Spawn (part);
+		}
+	}
+
+	[Command]
+	public void CmdPartDestroyed (GameObject part)
+	{
+		partCount--;
+		NetworkServer.Destroy (part);
+	}
+}
